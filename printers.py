7aa538578d8ad2ec49bedfad5054115e0bb903d4
hotrhoth <149301395+hotrhoth@users.noreply.github.com>
2024-04-27 15:33:00 +0700

    Add files via upload

---
+++ printers.py
@@ -0,0 +1,14 @@
+import pygame
+from typing import Tuple
+
+def leftAlignPrint(scr: pygame.Surface, ft: pygame.font.Font, text: str, pos: Tuple[int, int], color: Tuple[int, int, int] = (255, 255, 255)) -> None:
+    textObj = ft.render(text, True, color)
+    scr.blit(textObj, textObj.get_rect(center=[pos[0] + ft.size(text)[0]/2, pos[1]]))
+
+def centeredPrint(scr: pygame.Surface, ft: pygame.font.Font, text: str, pos: Tuple[int, int], color: Tuple[int, int, int] = (255, 255, 255)) -> None:
+    textObj = ft.render(text, True, color)
+    scr.blit(textObj, textObj.get_rect(center=pos))
+
+def rightAlignPrint(scr: pygame.Surface, ft: pygame.font.Font, text: str, pos: Tuple[int, int], color: Tuple[int, int, int] = (255, 255, 255)) -> None:
+    textObj = ft.render(text, True, color)
+    scr.blit(textObj, textObj.get_rect(center=[pos[0] - ft.size(text)[0] / 2, pos[1]]))
